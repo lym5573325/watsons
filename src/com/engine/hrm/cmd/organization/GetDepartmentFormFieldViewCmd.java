23b2982267e566139dc09c986443f1ba7ec43710
lym5573325 <362938069@qq.com>
2020-03-24 18:17:42 +0800

    1.删除部门卡片的入职/离职和人员卡片tab页  2.完成EDMS的传输

---
+++ src/com/engine/hrm/cmd/organization/GetDepartmentFormFieldViewCmd.java
@@ -0,0 +1,159 @@
+package com.engine.hrm.cmd.organization;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import weaver.conn.RecordSet;
+import weaver.general.BaseBean;
+import weaver.general.Util;
+import weaver.hrm.User;
+import weaver.hrm.definedfield.HrmDeptFieldManagerE9;
+import weaver.hrm.definedfield.HrmFieldComInfo;
+import weaver.hrm.definedfield.HrmFieldGroupComInfo;
+import weaver.systeminfo.SystemEnv;
+
+import com.api.browser.bean.SearchConditionItem;
+import com.api.hrm.bean.HrmFieldBean;
+import com.api.hrm.util.HrmFieldSearchConditionComInfo;
+import com.engine.common.biz.AbstractCommonCommand;
+import com.engine.common.entity.BizLogContext;
+import com.engine.core.interceptor.CommandContext;
+import com.engine.hrm.util.HrmOrganizationUtil;
+
+public class GetDepartmentFormFieldViewCmd extends AbstractCommonCommand<Map<String, Object>> {
+
+	public GetDepartmentFormFieldViewCmd(Map<String, Object> params, User user) {
+		this.user = user;
+		this.params = params;
+	}
+
+	@Override
+	public Map<String, Object> execute(CommandContext commandContext) {
+		new BaseBean().writeLog("GetDepartmentFormFieldViewCmd");
+		Map<String, Object> retmap = new HashMap<String, Object>();
+		List<Map<String, Object>> grouplist = new ArrayList<Map<String, Object>>();
+		Map<String, Object> groupitem = null;
+		List<Object> itemlist = null;
+		try {
+			String id = Util.null2String(params.get("id"));
+			int viewattr = 1;
+			
+			HrmFieldGroupComInfo HrmFieldGroupComInfo = new HrmFieldGroupComInfo();
+			HrmFieldComInfo HrmFieldComInfo = new HrmFieldComInfo();
+			HrmFieldSearchConditionComInfo hrmFieldSearchConditionComInfo = new HrmFieldSearchConditionComInfo();
+			SearchConditionItem searchConditionItem = null;
+			HrmFieldBean hrmFieldBean = null;
+			HrmDeptFieldManagerE9 hfm = new HrmDeptFieldManagerE9(5);
+			
+			hfm.getCustomData(Util.getIntValue(id));
+			List lsGroup = hfm.getLsGroup();
+			for (int tmp = 0; lsGroup != null && tmp < lsGroup.size(); tmp++) {
+				String groupid = (String) lsGroup.get(tmp);
+				List lsField = hfm.getLsField(groupid);
+				if (lsField.size() == 0)
+					continue;
+				if (hfm.getGroupCount(lsField) == 0)
+					continue;
+		  	if(!Util.null2String(HrmFieldGroupComInfo.getIsShow(groupid)).equals("1"))continue;
+				String grouplabel = HrmFieldGroupComInfo.getLabel(groupid);
+				itemlist = new ArrayList<Object>();
+				groupitem = new HashMap<String, Object>();
+				groupitem.put("title", SystemEnv.getHtmlLabelNames(grouplabel, user.getLanguage()));
+				groupitem.put("defaultshow", true);
+				for (int j = 0; lsField != null && j < lsField.size(); j++) {
+					String fieldid = (String) lsField.get(j);
+					String isuse = HrmFieldComInfo.getIsused(fieldid);
+					if (!isuse.equals("1"))continue;
+					String fieldname = HrmFieldComInfo.getFieldname(fieldid);
+					String fieldlabel = HrmFieldComInfo.getLabel(fieldid);
+					String fieldhtmltype = HrmFieldComInfo.getFieldhtmltype(fieldid);
+					String type = HrmFieldComInfo.getFieldType(fieldid);
+					String dmlurl = Util.null2String(HrmFieldComInfo.getFieldDmlurl(fieldid));
+					String fieldValue = hfm.getData(fieldname);
+					
+					if(id.length()>0 && fieldname.equals("showid")){
+						fieldValue = id;
+					}
+					
+					hrmFieldBean = new HrmFieldBean();
+					hrmFieldBean.setFieldid(fieldid);
+					hrmFieldBean.setFieldname(fieldname);
+					hrmFieldBean.setFieldlabel(fieldlabel);
+					hrmFieldBean.setFieldhtmltype(fieldhtmltype);
+					hrmFieldBean.setType(type);
+					hrmFieldBean.setIsFormField(true);
+					hrmFieldBean.setFieldvalue(fieldValue);
+					hrmFieldBean.setDmlurl(dmlurl);
+					hrmFieldBean.setIssystem("1");
+					searchConditionItem = hrmFieldSearchConditionComInfo.getSearchConditionItem(hrmFieldBean, user);
+					if (searchConditionItem.getBrowserConditionParam() != null) {
+						searchConditionItem.getBrowserConditionParam().setViewAttr(viewattr);
+					}
+					if(fieldname.equals("showorder")){
+						searchConditionItem.setPrecision(2);
+					}
+					searchConditionItem.setViewAttr(viewattr);
+					itemlist.add(searchConditionItem);
+				}
+
+				/*
+				if(tmp==0){
+					int resourceNum = 0;
+					RecordSet rs = new RecordSet();
+					
+					String sql = "SELECT COUNT(*) FROM hrmresource WHERE departmentid = "+ id +" and ( status =0 or status = 1 or status = 2 or status = 3)";
+					rs.executeSql(sql);
+					if(rs.next()){
+						resourceNum = rs.getInt(1);
+					}
+					
+					//4:解聘 5:离职 6:退休 7:无效
+					int resourceNum1 = 0;
+					 sql = "SELECT COUNT(*) FROM hrmresource WHERE departmentid = "+ id +" and status in(4,5,6,7)";
+					rs.executeSql(sql);
+					if(rs.next()){
+						resourceNum1 = rs.getInt(1);
+					}
+					hrmFieldBean = new HrmFieldBean();
+					hrmFieldBean.setFieldname("deptResourceInfo");
+					hrmFieldBean.setFieldlabel("382428");
+					hrmFieldBean.setFieldhtmltype("1");
+					hrmFieldBean.setType("1");
+					hrmFieldBean.setIsFormField(true);
+					hrmFieldBean.setFieldvalue(resourceNum+"/"+resourceNum1);
+					hrmFieldBean.setDmlurl("");
+					searchConditionItem = hrmFieldSearchConditionComInfo.getSearchConditionItem(hrmFieldBean, user);
+					searchConditionItem.setViewAttr(viewattr);
+					itemlist.add(searchConditionItem);
+				}
+				 */
+				groupitem.put("items", itemlist);
+				grouplist.add(groupitem);
+			}
+			RecordSet rs = new RecordSet();
+			String canceled = "";
+			String sql = "select canceled from hrmdepartment where id=" + id;
+			rs.executeSql(sql);
+			if(rs.next()){
+				canceled = Util.null2String(rs.getString("canceled"));
+			}
+			retmap.put("status", "1");
+			retmap.put("id", id);
+			retmap.put("titleInfo", HrmOrganizationUtil.getTitleInfo(id, "department", user));
+			retmap.put("formField", grouplist);
+			retmap.put("canceled", canceled.equals("1"));
+		} catch (Exception e) {
+			retmap.put("status", "-1");
+			retmap.put("message",  SystemEnv.getHtmlLabelName(382661,user.getLanguage()));
+			writeLog(e);
+		}
+		return retmap;
+	}
+
+	@Override
+	public BizLogContext getLogContext() {
+		return null;
+	}
+}
---
+++ src/com/engine/hrm/cmd/organization/GetTabsCmd.java
@@ -0,0 +1,88 @@
+package com.engine.hrm.cmd.organization;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import weaver.general.BaseBean;
+import weaver.general.Util;
+import weaver.hrm.User;
+import weaver.systeminfo.SystemEnv;
+import com.engine.common.biz.AbstractCommonCommand;
+import com.engine.common.entity.BizLogContext;
+import com.engine.core.interceptor.CommandContext;
+
+public class GetTabsCmd extends AbstractCommonCommand<Map<String, Object>> {
+
+    public GetTabsCmd(Map<String, Object> params, User user) {
+        this.user = user;
+        this.params = params;
+    }
+
+    @Override
+    public Map<String, Object> execute(CommandContext commandContext) {
+        new BaseBean().writeLog("111");
+        Map<String,Object> retmap = new HashMap<String,Object>();
+        try{
+            List<Map<String,Object>> tabs = new ArrayList<Map<String,Object>>();
+            Map<String,Object> tab = null;
+            String type = Util.null2String(params.get("type"));
+
+            if(type.equals("company")){
+                tab = new HashMap<String, Object>();
+                tab.put("key", "1");
+                tab.put("title", SystemEnv.getHtmlLabelName(33209,user.getLanguage()));
+                tabs.add(tab);
+
+                tab = new HashMap<String, Object>();
+                tab.put("key", "2");
+                tab.put("title", SystemEnv.getHtmlLabelName(17898,user.getLanguage()));
+                tabs.add(tab);
+            }else if(type.equals("subcompany")){
+                tab = new HashMap<String, Object>();
+                tab.put("key", "1");
+                tab.put("title", SystemEnv.getHtmlLabelName(32816,user.getLanguage()));
+                tabs.add(tab);
+
+                tab = new HashMap<String, Object>();
+                tab.put("key", "2");
+                tab.put("title", SystemEnv.getHtmlLabelName(17898,user.getLanguage()));
+                tabs.add(tab);
+
+                tab = new HashMap<String, Object>();
+                tab.put("key", "3");
+                tab.put("title", SystemEnv.getHtmlLabelName(17587,user.getLanguage()));
+                tabs.add(tab);
+            }else if(type.equals("department")){
+                tab = new HashMap<String, Object>();
+                tab.put("key", "1");
+                tab.put("title", SystemEnv.getHtmlLabelName(16289,user.getLanguage()));
+                tabs.add(tab);
+
+                tab = new HashMap<String, Object>();
+                tab.put("key", "2");
+                tab.put("title", SystemEnv.getHtmlLabelName(17587,user.getLanguage()));
+                tabs.add(tab);
+
+                tab = new HashMap<String, Object>();
+                tab.put("key", "3");
+                tab.put("title", SystemEnv.getHtmlLabelName(179,user.getLanguage()));
+                //tabs.add(tab);
+            }
+            retmap.put("status", "1");
+            retmap.put("tabs", tabs);
+        }catch (Exception e) {
+            retmap.put("status", "-1");
+            retmap.put("message",  SystemEnv.getHtmlLabelName(382661,user.getLanguage()));
+            writeLog(e);
+        }
+
+        return retmap;
+    }
+
+    @Override
+    public BizLogContext getLogContext() {
+        return null;
+    }
+}
